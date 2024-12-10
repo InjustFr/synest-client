2702edcb0df41ba98e8e6f8074dc7bcbc67cdf69
InjustFr <gomen565@gmail.com>
2024-12-10 08:48:22 +0100

    feat(config): add .env files to configurate app

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,37 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
-import mkcert from 'vite-plugin-mkcert'
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vite.dev/config/
-export default defineConfig({
-    server: {
-        port: 7000,
-        proxy: {
-            '/api': {
-                target: 'https://localhost:6001',
-                secure: false,
-                rewrite: (path: string) => path.replace(/^\/api/, ''),
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), 'VITE_') as unknown as {
+        readonly VITE_PORT: number;
+        readonly VITE_API_URL: string;
+    };
+
+    return {
+        server: {
+            port: env.VITE_PORT,
+            proxy: {
+                '/api': {
+                    target: env.VITE_API_URL,
+                    secure: false,
+                    rewrite: (path: string) => path.replace(/^\/api/, ''),
+                },
             },
         },
-    },
-    plugins: [
-        vue(),
-        vueDevTools(),
-        mkcert(),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        plugins: [
+            vue(),
+            vueDevTools(),
+            mkcert(),
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            },
         },
-    },
+    };
 });
